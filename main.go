2470470afb5da6207bd35be9f147eb66b1b9dc4d
Tomas Kral <tkral@redhat.com>
2016-05-20 10:00:31 +0200

    convert to versioned objects and to list instead of template

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import (
 	"fmt"
 
 	"github.com/kadel/henge/pkg/generate/dockercompose"
+	"github.com/openshift/origin/pkg/generate/app"
+	kapi "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apimachinery/registered"
+
+	// Install OpenShift APIs
+	_ "github.com/openshift/origin/pkg/build/api/install"
+	_ "github.com/openshift/origin/pkg/deploy/api/install"
+	_ "github.com/openshift/origin/pkg/image/api/install"
+	_ "github.com/openshift/origin/pkg/route/api/install"
+	_ "github.com/openshift/origin/pkg/template/api/install"
 )
 
 func main() {
@@ -16,6 +26,16 @@ func main() {
 		return
 	}
 
-	fmt.Printf("%#v", template)
+	if errs := app.AsVersionedObjects(template.Objects, kapi.Scheme, kapi.Scheme, registered.EnabledVersions()...); len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Printf("error: %v\n", err)
+		}
+	}
+
+	// make it List instead of Template
+	list := &kapi.List{Items: template.Objects}
+	for _, obj := range list.Items {
+		fmt.Printf("%#v\n", obj)
+	}
 
 }
