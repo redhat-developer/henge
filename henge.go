3de038131742d61bc9e88ed1aa76b565acbe6bde
Ratnadeep Debnath <rtnpro@gmail.com>
2016-06-07 15:44:41 +0530

    Bring back ifFileExists func removed during rebase.

--- henge.go
+++ henge.go
@@ -8,8 +8,23 @@ import (
 	"github.com/rtnpro/henge/pkg/transformers"
 )
 
+// Loop over a array of filepaths and check if it exists
+// if it exists check if it is not a directory.
+func ifFileExists(files []string) error {
+	for _, filename := range files {
+		fileInfo, err := os.Stat(filename)
+		if err != nil {
+			return fmt.Errorf("main: file %q not found", filename)
+		}
+		if fileInfo.IsDir() {
+			return fmt.Errorf("main: %q is a directory", filename)
+		}
+	}
+	return nil
+}
+
 func main() {
-	provider := flag.String("provider", "openshift", "Target provider")
+	provider := flag.String("provider", "", "Target provider")
 
 	flag.Parse()
 
@@ -20,7 +35,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	err := transformers.Transform(*provider, flag.Args()[0:]...)
+	err = transformers.Transform(*provider, flag.Args()[0:]...)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
