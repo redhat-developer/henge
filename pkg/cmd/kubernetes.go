70bc4360de0bcce3fe541bde4768a8719fdd8640
Shubham Minglani <shubham@linux.com>
2016-07-10 20:27:49 +0530

    refactor parsing of commands, implement providers as subcommands, fix #43

---
+++ pkg/cmd/kubernetes.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/redhat-developer/henge/pkg/transformers/kubernetes"
+	"github.com/redhat-developer/henge/pkg/types"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func kubernetesCmd(vals *types.CmdValues) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "kubernetes",
+		Short: "convert to Kubernetes artifacts",
+		Long:  "To convert the docker-compose.yml file in the current directory to kubernetes' artifacts",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			errorIfFileDoesNotExist(vals)
+
+			list, err := kubernetes.Transform(vals)
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+			err = kubernetes.PrintList(list, vals)
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+		},
+	}
+
+	cmd = addProviderFlags(cmd, vals)
+	return cmd
+}
